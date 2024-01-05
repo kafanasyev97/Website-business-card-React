89e6ac11ade08be534671bf8e6b48523b364bcf0
Kirill <kirillmas222@gmail.com>
2024-01-05 04:17:01 +0400

    [fix] Remove placeholder and added choise class

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,31 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const Info = () => {
   const [imageLoaded, setImageLoaded] = useState(false);
 
-  useEffect(() => {
-    const image = new Image();
-    image.src = "./images/kirill2-transformed.png";
-    image.onload = () => {
-      setImageLoaded(true);
-    };
-  }, []);
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <section className="main__section">
       <div className="container">
-        <div className="info">
-          {!imageLoaded && (
-            <div
-              style={{ position: "relative", width: "585px", height: "700px" }}
-            ></div>
-          )}
+        <div className={imageLoaded ? "info" : "none__display"}>
           <div className="info__img">
             <img
               src="./images/kirill2-transformed.png"
               alt="img"
               className="info__img-main"
-              rel="image"
-              style={{ display: imageLoaded ? "block" : "none" }}
+              onLoad={handleImageLoad}
             />
           </div>
           <div className="info__text">
